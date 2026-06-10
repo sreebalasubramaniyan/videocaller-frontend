058128130c998e99c8166bfd194c51879d52137f
sreebalasubramaniyan <sreebalasubramaniyan682@gmail.com>
2026-06-10 12:28:07 +0530

    Updated URL

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000';
+const SOCKET_URL = 'https://videocaller-backend.onrender.com';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
